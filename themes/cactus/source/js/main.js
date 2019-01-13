460f3ad6edc92bb90173607815afd73e96c36a40
Angelore <angelore.main@gmail.com>
2019-01-13 23:26:42 +0300

    Fixed issues with the top scroller

--- themes/cactus/source/js/main.js
+++ themes/cactus/source/js/main.js
@@ -12,7 +12,6 @@ if (!!$.prototype.justifiedGallery) {
 
 
 $(document).ready(function() {
-
   /**
    * Shows the responsive navigation menu on mobile.
    */
@@ -27,53 +26,15 @@ $(document).ready(function() {
    */
   if ($(".post").length) {
     var menu = $("#menu");
-    var nav = $("#menu > #nav");
-    var menuIcon = $("#menu-icon, #menu-icon-tablet");
 
-    /**
-     * Display the menu on hi-res laptops and desktops.
-     */
-    if ($(document).width() >= 1440) {
-      menu.css("visibility", "visible");
-      menuIcon.addClass("active");
-    }
-
-    /**
-     * Display the menu if the menu icon is clicked.
-     */
-    menuIcon.click(function() {
-      if (menu.css("visibility") === "hidden") {
-        menu.css("visibility", "visible");
-        menuIcon.addClass("active");
-      } else {
-        menu.css("visibility", "hidden");
-        menuIcon.removeClass("active");
-      }
-      return false;
-    });
-
-    /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
-     */
     if (menu.length) {
       $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
-
-        // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 50) {
-          nav.show();
-        } else if (nav.is(":visible") && topDistance > 100) {
-          nav.hide();
-        }
-
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
+        var topDistance = $("html").scrollTop();
+        var isScrolling = $('#top-icon')[0].IsScrolling;
+        if (topDistance < 200 ) {
+          $("#top-icon").hide(200);
+        } else if (topDistance > 400 && !isScrolling === true) {
+          $("#top-icon").show(200);
         }
       });
     }
